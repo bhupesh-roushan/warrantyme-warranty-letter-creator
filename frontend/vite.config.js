200efa6257aa585d74d41ed23bfbf6f606b0c3a4
bhupesh-roushan <roushan.bhupesh@gmail.com>
2025-03-30 10:31:48 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,5 +7,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0", // Ensure the app is accessible externally
     port: process.env.PORT || 5173, // Use Render's assigned port
+    strictPort: true,
+    allowedHosts: ["warrantyme-warranty-letter-creator-f2.onrender.com"],
   },
 })
